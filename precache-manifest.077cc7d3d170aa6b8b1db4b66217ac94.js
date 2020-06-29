2c5024f4e3f6d23f9888702d14da0b2b140059ab
Sourav Mondal <souravmondal0341@gmail.com>
2020-06-29 20:11:00 +0530

    Updates

---
+++ precache-manifest.077cc7d3d170aa6b8b1db4b66217ac94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d9b31ba0f610976735859bfdf779b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "14ade1d4df9699b14cd6",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ade1d4df9699b14cd6",
+    "url": "/static/js/main.e94bd377.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "5514c9ddd6051c71456c9e301dfbd3d2",
+    "url": "/static/media/resume.5514c9dd.pdf"
+  }
+]);
